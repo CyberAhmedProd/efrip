e49ee6c143e2076f5a736849e6bdb70c3416edba
CyberAhmedProd <gharbiahmedpro@gmail.com>
2021-12-02 14:21:35 +0100

    fix & comment some code in order list vue

--- src/app/main/apps/order/order-list/order-list.service.ts
+++ src/app/main/apps/order/order-list/order-list.service.ts
@@ -2,9 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 import { Order } from 'app/auth/models';
-
 import { environment } from 'environments/environment';
-
 import { BehaviorSubject, Observable } from 'rxjs';
 interface MyData {
   success : number ;
@@ -15,7 +13,6 @@ export class OrderListService implements Resolve<any> {
   rows: any;
   onDatatablessChanged: BehaviorSubject<any>;
   currentUser : any
-
   /**
    * Constructor
    *
@@ -25,7 +22,6 @@ export class OrderListService implements Resolve<any> {
     // Set the defaults
     this.onDatatablessChanged = new BehaviorSubject({});
   }
-
   /**
    * Resolver
    *
@@ -40,7 +36,6 @@ export class OrderListService implements Resolve<any> {
       }, reject);
     });
   }
-
   /**
    * Get rows
    */
@@ -60,45 +55,36 @@ export class OrderListService implements Resolve<any> {
       }, reject);
     });
   }
- 
-
+  //Delete the order from the database & add to history
   deleteOrder(id){
-    return this._httpClient.delete(`${environment.apiDistant}/api/order/`+id,{
-        
-    }).subscribe(data => {
-      console.log(data);
+    return this._httpClient.delete(`${environment.apiDistant}/api/order/`+id,{        
+    })
+    //test phase need to revove this in production mode
+    .subscribe(data => {console.log(data);
     })
   }
+  // Add order to the shipping status & add the trasaction to the history
   onShipping(order: Order){
- 
     return new Promise((resolve, reject) => {
       this._httpClient.put(`${environment.apiDistant}/api/order/shipping/${order.id}`,order).subscribe((response: any) => {
-       
-        
         resolve(response);
       }, reject);
     });
   }
+  // Add order in status on hold & prepare to go in shipping & add status to history
   onHold(order: Order){
- 
     return new Promise((resolve, reject) => {
       this._httpClient.put(`${environment.apiDistant}/api/order/hold/${order.id}`,order).subscribe((response: any) => {
-       
-        
         resolve(response);
       }, reject);
     });
   }
+  // add order in status on delevered status & client checked the delevery & add ths status to the history
   onDelivered(order: Order){
- 
     return new Promise((resolve, reject) => {
       this._httpClient.put(`${environment.apiDistant}/api/order/delivered/${order.id}`,order).subscribe((response: any) => {
-       
-        
         resolve(response);
       }, reject);
     });
   }
-  
-
 }
